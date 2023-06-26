4ce40d1aae1c438e21fdbdfb542dd95281b3b251
Gareth Grindeland <gt.grindeland@gmail.com>
2023-06-26 11:02:21 -0700

    add initial project files

---
+++ social-media-clone/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      {/* <Header />
+      <TicketList /> */}
+    </React.Fragment>
+  );
+}
+
+export default App;
